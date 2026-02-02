8cb59f02b6fa625bf86c2dcaf6dea28d58401838
Lntano <0xlntano@gmail.com>
2026-02-02 15:57:41 +0800

    增加 thrd_pool.c，atomic.h，修改 atomic.h

--- thrd_pool.h
+++ thrd_pool.h
@@ -1,5 +1,5 @@
-#ifndef _THRD_POOL_H
-#define _THRD_POOL_H
+#ifndef _THREAD_POOL_H
+#define _THREAD_POOL_H
 
 typedef struct thrdpool_s thrdpool_t;
 typedef void (*handler_pt)(void *);
@@ -10,12 +10,17 @@ extern "C"
 #endif
 
 // 对称处理
+// 创建线程池
 thrdpool_t *thrdpool_create(int thrd_count);
 
+// 销毁线程池
 void thrdpool_terminate(thrdpool_t * pool);
 
+// 投递任务
+// 向线程池的任务队列中添加一个新任务。这是一个生产者操作。
 int thrdpool_post(thrdpool_t *pool, handler_pt func, void *arg);
 
+// 等待线程池的退出
 void thrdpool_waitdone(thrdpool_t *pool);
 
 #ifndef __cplusplus
